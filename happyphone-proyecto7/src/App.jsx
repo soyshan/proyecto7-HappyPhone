6b82b5e34d05cad0072571da24992bbf0f47fc77
soyshan <shanbu131@gmail.com>
2024-01-17 13:30:04 +0100

    actualizando rama por errores

---
+++ happyphone-proyecto7/src/App.jsx
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { ContactForm } from './pages/ContactForm';
+import { SobreNosotros } from './pages/SobreNosotros';
+import { Home } from './pages/Home';
+import { Navigation } from './components/Navigation'
+import { AboutHappyPhone } from './pages/AboutHappyPhone'
+import { WorkWithUs } from './pages/WorkWithUs'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<Navigation />} />
+        <Route path="/" element={<Home />} />
+        <Route path="sobre-nosotros" element={<SobreNosotros />} />
+        <Route path="contactform" element={<ContactForm />} />
+        <Route path="abouthappyphone" element={<AboutHappyPhone />} />
+        <Route path="workwithus" element={<WorkWithUs />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
