3eac2b1ae288972054cdfd8beb61f68493736e9f
soyshan <shanbu131@gmail.com>
2024-01-17 09:43:15 +0100

    fixing to App

--- happyphone-proyecto7/src/App.js
+++ happyphone-proyecto7/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import './css/style.css';
+import './css/cart.css';
+import './css/main.css';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="frameBuscar">
+        <input type="text" minLength="3" maxLength="15" id="inpBusq" style={{ width: '150px' }} />
+      </div>
+      <div className="cart">
+        <li>
+          <a href="./html/carrito.html">
+            <img className="logo-shoppingcart" src="./img/shoppingcart.png" alt="shoppingcart" />
+            <div id="cart-amount" className="cart-amount">0</div>
+          </a>
+        </li>
+      </div>
+      <Header />
+      <main className="main-offer" id="main-offer">
+        <figure>
+          <a href="./html/product.html">
+            <img className="main-image" src="./img/happyinicio.svg" alt="Foto con oferta de movil de lanzamiento" />
+          </a>
+        </figure>
+        <article className="main-text">
+          <h1>Consigue tu HappyZ Flip con HappyPhone</h1>
+          <h4>Aprovecha nuestra oferta de lanzamiento</h4>
+          <a href="./html/product.html"><button className="main-button">¡Descúbrelo!</button></a>
+        </article>
+      </main>
+      {/* ... (Resto de tu contenido) */}
+      <Footer />
     </div>
   );
 }
