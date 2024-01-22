6430dd2a7af688ab2203d3adbadc08edc26850fd
Ermapla <ermapla@gmail.com>
2024-01-22 15:13:47 +0100

    cart page

--- happyphone-proyecto7/src/App.js
+++ happyphone-proyecto7/src/App.js
@@ -5,6 +5,14 @@ import { Home } from './pages/Home';
 import { Navigation } from './components/Navigation'
 import { AboutHappyPhone } from './pages/AboutHappyPhone'
 import { WorkWithUs } from './pages/WorkWithUs'
+import { Terms } from './pages/Terms'
+// import { Equipo } from './pages/MoreInformation'
+import { Footer }from './components/Footer/FooterComponent';
+import MovilUnico from './components/MovilUnico';
+import { Thanks } from './pages/Thanks';
+import { Carrito } from './pages/Carrito';
+
+
 
 function App() {
   return (
@@ -13,14 +21,24 @@ function App() {
         <Navigation />
         <Routes>
           <Route index element={<Home />} />
-          <Route path="sobre-nosotros" element={<SobreNosotros />} />
+          <Route path="/movil/:id" element={<MovilUnico />} />
+          <Route path="sobreNosotros" element={<SobreNosotros />} />
           <Route path="contactform" element={<ContactForm />} />
           <Route path='abouthappyphone' element={<AboutHappyPhone />} />
           <Route path='workwithus' element={<WorkWithUs />} />
+          <Route path='terms' element={<Terms/>} />
+
+          <Route path='Thanks' element={<Thanks/>} />
+          <Route path='carrito' element={<Carrito />} />
+         
+
+
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
--- happyphone-proyecto7/src/pages/Home.js
+++ happyphone-proyecto7/src/pages/Home.js
@@ -1,36 +1,37 @@
-import React from 'react';
-// import FooterComponent from './components/Footer';
-import { ReactComponent as HappyInicio } from '../assets/happyinicio.svg';
+import React, { useState, useEffect } from 'react';
+import HappyInicio from '../assets/happyinicio.svg';
+import Moviles from '../components/Moviles.js';
+import { Link } from 'react-router-dom';
 
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('./json/catalogo.json')
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.moviles);
+              });    
+    
+  }, []);
+
   return (
     <>
-    
       {/* OFERTA PRINCIPAL */}
       <main className="main-offer" id="main-offer">
         <figure>
-          <a href="./html/product.html"><HappyInicio className="main-image" alt="Foto con oferta de móvil de lanzamiento" /></a>
+          <Link to="./Home"><img src={HappyInicio} className="main-image" alt="Foto con oferta de móvil de lanzamiento" /></Link>
         </figure>
         <article className="main-text">
           <h1>Consigue tu HappyZ Flip con HappyPhone</h1>
           <h4>Aprovecha nuestra oferta de lanzamiento</h4>
-          <a href="./html/product.html"><button className="main-button">¡Descúbrelo!</button></a>
+          <Link to={`/movil/1`}><button className="main-button">¡Descúbrelo!</button></Link>
         </article>
       </main>
 
-      {/* CAROUSEL PRODUCTOS */}
-      <section className="carousel-products" id="carousel-products">
-        {/* Aquí deberías incluir tu lógica para mostrar productos en el carousel */}
-      </section>
-      <section className="productoBuscado" id="productoBuscado">
-        {/* Aquí deberías incluir tu lógica para mostrar productos buscados */}
-      </section>
-      <br />
+      <Moviles />
 
-      {/* FOOTER */}
-      {/* <FooterComponent /> */}
     </>
   );
 };
-
