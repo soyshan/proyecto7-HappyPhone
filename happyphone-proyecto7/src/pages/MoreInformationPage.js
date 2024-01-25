1fc0b8276bf1e60b5628c9c3eefa4b778040a352
natalia9519 <nataliayda@gmail.com>
2024-01-25 09:57:28 +0100

    Little changes css and words

--- happyphone-proyecto7/src/pages/MoreInformationPage.js
+++ happyphone-proyecto7/src/pages/MoreInformationPage.js
@@ -79,88 +79,99 @@
 
 
 
-import React from "react";
+// import React from "react";
+// import MoreInformation from "../components/OurTeam/MoreInformation";
+// import "../components/OurTeam/moreinformation.css";  // Importa el archivo CSS
+// import andreImage from "../assets/equipo/andre.jpeg";
+// import daniImage from "../assets/equipo/dani.jpeg";
+// import luisImage from "../assets/equipo/luis.jpeg";
+
+// export const MoreInformationPage = () => {
+//   const teamMembers = [
+//     {
+//       id: 1,
+//       imgSrc: andreImage,
+//       description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
+//       profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
+//       pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
+//       practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
+//       sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
+//       expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
+//       momento con risas, descubrimientos y emociones genuinas.`
+//     },
+//     {
+//       id: 2,
+//       imgSrc: daniImage,
+//       description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
+//       profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
+//       pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
+//       practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
+//       sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
+//       expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
+//       momento con risas, descubrimientos y emociones genuinas.`
+//     },
+//     {
+//       id: 3,
+//       imgSrc: luisImage,
+//       description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
+//       profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
+//       pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
+//       practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
+//       sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
+//       expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
+//       momento con risas, descubrimientos y emociones genuinas.`
+//     },
+
+//     {
+//       id: 4,
+//       imgSrc: andreImage,
+//       description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
+//       profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
+//       pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
+//       practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
+//       sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
+//       expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
+//       momento con risas, descubrimientos y emociones genuinas.`
+//     },
+
+//     {
+//       id: 5,
+//       imgSrc: luisImage,
+//       description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
+//       profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
+//       pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
+//       practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
+//       sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
+//       expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
+//       momento con risas, descubrimientos y emociones genuinas.`
+//     },
+//     // Agrega otros miembros del equipo aquí
+//   ];
+
+//   // Define la función mostrarDescripcion aquí
+//   const mostrarDescripcion = (id) => {
+//     // Lógica para mostrar la descripción
+//     console.log(`Mostrar descripción para el miembro con ID: ${id}`);
+//   };
+
+//   return (
+//     <div className="galeria">
+//       <h1>Nuestro equipo</h1>
+//       <MoreInformation teamMembers={teamMembers} mostrarDescripcion={mostrarDescripcion} />
+//     </div>
+//   );
+// };
+
+// MoreInformationPage.js
+import React from 'react';
 import MoreInformation from "../components/OurTeam/MoreInformation";
-import "../components/OurTeam/moreinformation.css";  // Importa el archivo CSS
-import andreImage from "../assets/equipo/andre.jpeg";
-import daniImage from "../assets/equipo/dani.jpeg";
-import luisImage from "../assets/equipo/luis.jpeg";
 
 export const MoreInformationPage = () => {
-  const teamMembers = [
-    {
-      id: 1,
-      imgSrc: andreImage,
-      description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
-      profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
-      pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
-      practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
-      sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
-      expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
-      momento con risas, descubrimientos y emociones genuinas.`
-    },
-    {
-      id: 2,
-      imgSrc: daniImage,
-      description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
-      profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
-      pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
-      practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
-      sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
-      expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
-      momento con risas, descubrimientos y emociones genuinas.`
-    },
-    {
-      id: 3,
-      imgSrc: luisImage,
-      description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
-      profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
-      pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
-      practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
-      sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
-      expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
-      momento con risas, descubrimientos y emociones genuinas.`
-    },
-
-    {
-      id: 4,
-      imgSrc: andreImage,
-      description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
-      profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
-      pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
-      practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
-      sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
-      expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
-      momento con risas, descubrimientos y emociones genuinas.`
-    },
-
-    {
-      id: 5,
-      imgSrc: luisImage,
-      description:`Andrea, una buscadora incansable de aventuras en la vida y en el mundo digital. En suviaje de reinvención
-      profesional hacia el mundo de la tecnología, se sumerge en el desarrollo web y la ciberseguridad con una
-      pasión contagiosa. Cuando no está inmersa en líneas de código, la encontrarás explorando senderos naturales,
-      practicando yoga para mantener equilibrio y descubriendo tesoros escondidos en nuevos lugares. Su risa es su
-      sello distintivo, capaz de iluminar cualquier espacio, pero también lleva consigo la nobleza de lágrimas que
-      expresan tanto alegría desbordante como profundidad emocional. Para ella, la vida se trata de llenar cada
-      momento con risas, descubrimientos y emociones genuinas.`
-    },
-    // Agrega otros miembros del equipo aquí
-  ];
-
-  // Define la función mostrarDescripcion aquí
-  const mostrarDescripcion = (id) => {
-    // Lógica para mostrar la descripción
-    console.log(`Mostrar descripción para el miembro con ID: ${id}`);
-  };
-
   return (
-    <div className="galeria">
-      <h1>Nuestro equipo</h1>
-      <MoreInformation teamMembers={teamMembers} mostrarDescripcion={mostrarDescripcion} />
+    <div>
+      {/* <h1>Información Adicional</h1> */}
+      <MoreInformation />
     </div>
   );
 };
-
-
 
