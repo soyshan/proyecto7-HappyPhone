0b2449189ac0afd5403847c845a31ca0af1753af
soyshan <shanbu131@gmail.com>
2024-01-25 10:43:53 +0100

    update moreinformation page and some styles

--- happyphone-proyecto7/src/components/OurTeam/MoreInformation.js
+++ happyphone-proyecto7/src/components/OurTeam/MoreInformation.js
@@ -1,105 +1,59 @@
-// components/MoreInformation.js
-// import React from "react";
-// import "./moreinformation.css";
-
-
-// const MoreInformation = ({ member, mostrarDescripcion }) => {
-//   return (
-//     <>
-      
-//       <div className=" equipo-img" >
-//         {teamMembers.map((member) => (
-//             <div key={member.id} className="foto equipo-img" onClick={() => mostrarDescripcion(member.id)}>
-//             <img src={member.imgSrc}  alt={`Team Member ${member.id}`}  style={{ width: "200px", height: "auto" }}/>
-//             <p className="descripcion">{member.description}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default MoreInformation;
-
-
-// components/MoreInformation.js
-
-// components/OurTeam/MoreInformation.js
-
-// import React, { useState, useEffect } from "react";
-// import TeamMember from "./TeamMember"; // Asegúrate de tener el componente TeamMember en el mismo directorio
-
-// const MoreInformation = () => {
-//   const [teamMembers, setTeamMembers] = useState([]);
-
-//   useEffect(() => {
-//     const fetchTeamMembers = async () => {
-//       try {
-//         const response = await fetch(`${process.env.PUBLIC_URL}/json/teamMembers.json`);
-//         if (!response.ok) {
-//           throw new Error('Error al cargar el archivo JSON');
-//         }
-//         const data = await response.json();
-//         setTeamMembers(data);
-//       } catch (error) {
-//         console.error('Error:', error.message);
-//       }
-//     };
-
-//     fetchTeamMembers();
-//   }, []);
-
-//   return (
-//     <section className="section-team-members">
-//       <h1 className="h1-team-members">Equipo</h1>
-//       <div className="div-team-members">
-//         <div className="team-members">
-//           {teamMembers.map(member => (
-//             <TeamMember key={member.id} member={member} />
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default MoreInformation;
-
-
-// components/OurTeam/MoreInformation.js
 import React, { useState, useEffect } from "react";
+import "./moreinformation.css";
 
 const MoreInformation = () => {
   const [teamMembers, setTeamMembers] = useState([]);
 
   useEffect(() => {
-    fetch('/json/teamMembers.json')  // La barra inicial indica la raíz del servidor
-      .then(response => response.json())
-      .then(datos => {
-        setTeamMembers(datos);
-      })
-      .catch(error => {
-        console.error('Error:', error.message);
-      });
-  }, []);
+  const OurTeam = async () => {
+    try {
+    const response = await import('./teamMember.json')
+    const filteredItems = response.default.teamMembers || [];
+    setTeamMembers(filteredItems.map(member => ({ ...member, isHovered: false })));
+    console.log("teamMembers loaded:", teamMembers);
+ } catch (error) {
+  console.error("Error al cargar teamMember.json", error);
+ }
+}
+console.log("OurTeam function called");
+ OurTeam()
+ // eslint-disable-next-line
+ }, []);
 
-
-
-  return (
-    <section className="section-team-members">
-      <h1 className="h1-team-members">Equipo</h1>
-      <div className="div-team-members">
-        <div className="team-members">
-          {teamMembers.map(member => (
-            <div key={member.id} className="team-member">
-              <img src={`${process.env.PUBLIC_URL}/imgJson/${member.imgSrc}`} alt={member.imgSrc} />
-              <p>{member.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+ const handleMouseEnter = (id) => {
+  const updatedTeamMembers = teamMembers.map((member) =>
+    member.id === id ? { ...member, isHovered: true } : member
   );
+  setTeamMembers(updatedTeamMembers);
 };
 
-export default MoreInformation;
+const handleMouseLeave = (id) => {
+  const updatedTeamMembers = teamMembers.map((member) =>
+    member.id === id ? { ...member, isHovered: false } : member
+  );
+  setTeamMembers(updatedTeamMembers);
+};
+
+ return (
+  <div className="equipo-img-container">
+      {teamMembers.map((member) => (
+        <div
+          key={member.id}
+          className={`equipo-img-item ${member.isHovered ? "hovered" : ""}`}
+          onMouseEnter={() => handleMouseEnter(member.id)}
+          onMouseLeave={() => handleMouseLeave(member.id)}
+          
+        >
+        <img src={`/imgJson/${member.imgSrc}`} alt={`Team Member ${member.id}`} className="equipo-img" style={{ width: "200px", height: "auto" }} />
+
+        <div className="descripcion-overlay">
+            <p className="descripcion">{member.description}</p>
+          </div>
+      </div>
+    ))}
+  </div>
+);
+};
+
+
+export default MoreInformation
--- happyphone-proyecto7/src/pages/MoreInformationPage.js
+++ happyphone-proyecto7/src/pages/MoreInformationPage.js
@@ -164,12 +164,12 @@
 
 // MoreInformationPage.js
 import React from 'react';
-import MoreInformation from "../components/OurTeam/MoreInformation";
+import  MoreInformation  from "../components/OurTeam/MoreInformation";
 
 export const MoreInformationPage = () => {
   return (
     <div>
-      {/* <h1>Información Adicional</h1> */}
+      <h1 className='h1-our-team'>Nuestro Equipo</h1>
       <MoreInformation />
     </div>
   );
